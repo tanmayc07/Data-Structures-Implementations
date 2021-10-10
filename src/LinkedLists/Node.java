921c599c765ae3bf4be36256c599e9bd3294f50d
tanmayc07 <tanmayc32@gmail.com>
2021-10-10 21:29:40 +0530

    Add the Node class in its own file

---
+++ src/LinkedLists/Node.java
@@ -0,0 +1,11 @@
+/*
+ * Author - @tanmayc07
+ * Since Jetbrains IntelliJ was not letting me have multiple node class
+ * in different files due to my project structure, I added it to a seperate file ~*_*~
+ */
+package LinkedLists;
+
+public class Node {
+        int data;
+        LinkedLists.Node next;
+}
